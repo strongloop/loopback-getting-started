2ee043a4c4086aa3b31f0535744ead51f77b1a6d
crandmck <rand@strongloop.com>
2015-01-08 13:56:25 -0800

    Changes for mw phases

--- server/server.js
+++ server/server.js
@@ -3,31 +3,13 @@ var boot = require('loopback-boot');
 
 var app = module.exports = loopback();
 
-// Set up the /favicon.ico
-app.use(loopback.favicon());
-
-// request pre-processing middleware
-app.use(loopback.compress());
-
-// -- Add your pre-processing middleware here --
-
-// boot scripts mount components like REST API
+// Bootstrap the application, configure models, datasources and middleware.
+// Sub-apps like REST API are mounted via boot scripts.
 boot(app, __dirname);
 
-// -- Mount static files here--
-// All static middleware should be registered at the end, as all requests
-// passing the static middleware are hitting the file system
-// Example:
-var path = require('path');
-app.use(loopback.static(path.resolve(__dirname, '../client')));
-
-// Requests that get this far won't be handled
-// by any middleware. Convert them into a 404 error
-// that will be handled later down the chain.
-app.use(loopback.urlNotFound());
-
-// The ultimate error handler.
-app.use(loopback.errorHandler());
+app.use('/express_status', function(req, res, next) {
+  res.json({ running: true });
+});
 
 app.start = function() {
   // start the web server
