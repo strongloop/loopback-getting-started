919e80e07c948536b5c81b76246ced313526e7c5
Ryan Graham <r.m.graham@gmail.com>
2016-05-06 12:20:34 -0700

    update copyright notices and license

--- server/boot/routes.js
+++ server/boot/routes.js
@@ -1,3 +1,8 @@
+// Copyright IBM Corp. 2014. All Rights Reserved.
+// Node module: loopback-getting-started
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
 module.exports = function(app) {
   // Install a "/ping" route that returns "pong"
   app.get('/ping', function(req, res) {
--- server/server.js
+++ server/server.js
@@ -1,3 +1,8 @@
+// Copyright IBM Corp. 2014. All Rights Reserved.
+// Node module: loopback-getting-started
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
 var loopback = require('loopback');
 var boot = require('loopback-boot');
 
