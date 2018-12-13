f6ae861ffbdcc7abbef7cdd25311909b26e2d12f
Miroslav Bajtoš <mbajtoss@gmail.com>
2018-12-13 13:56:22 +0100

    fix the issue

--- common/models/coffee-shop-dispatcher.js
+++ common/models/coffee-shop-dispatcher.js
@@ -1,39 +1,46 @@
-var loopback = require('loopback').PersistedModel;
+const app = require('../../server/server');
+
+
 module.exports = function(CoffeeDispatcher) {
-  Coffee.find = function(ds, filter, cb) {
+  app.once('ready', () => defineRemoteApi);
+
+  CoffeeDispatcher.find = function(ds, filter, cb) {
     var model = this.findModelForDataSource(ds);
     model.find(filter, cb);
   };
 
-  // a modified copy of remoting metadata from loopback/lib/persisted-model.js
-  // remoteMethod is 
-  Coffee.remoteMethod('find', {
-    isStatic: true,
-    description: 'Find all instances of the model matched by filter from the data source',
-    accessType: 'READ',    
-    accepts: [
-     {arg: 'ds', type: 'string', description: 'Name of the datasource to use' },
-     {arg: 'filter', type: 'object', description: 'Filter defining fields, where, orderBy, offset, and limit'}
-    ],
-    returns: {arg: 'data', type: [typeName], root: true},
-    http: {verb: 'get', path: '/'}
-  });
-  // TODO: repeat the above for all methods you want to expose this way
+  function defineRemoteApi() {
+    const Coffee = app.models.Coffee;
 
-  Coffee.findModelForDataSource = function(ds) {
+    // a modified copy of remoting metadata from loopback/lib/persisted-model.js
+    // remoteMethod is
+    CoffeeDispatcher.remoteMethod('find', {
+      isStatic: true,
+      description: 'Find all instances of the model matched by filter from the data source',
+      accessType: 'READ',
+      accepts: [
+       {arg: 'ds', type: 'string', description: 'Name of the datasource to use' },
+       {arg: 'filter', type: 'object', description: 'Filter defining fields, where, orderBy, offset, and limit'}
+      ],
+      returns: {arg: 'data', type: [Coffee], root: true},
+      http: {verb: 'get', path: '/'}
+    });
+    // TODO: repeat the above for all methods you want to expose this way
+  }
+
+  CoffeeDispatcher.findModelForDataSource = function(ds) {
     var app = this.app;
     var ds = ds && app.dataSources[ds] || app.dataSources.default;
 
     var modelName = this.modelName + '-' + ds;
-    var model = loopback.findModel(modelName);
+    var model = app.registry.findModel(modelName);
     if (!model) {
-      model = loopback.createModel(
-        modelName, 
+      model = app.registry.createModel(
+        modelName,
         {},
-        { base: this.modelName });
+        { base: Coffee.modelName });
     }
     console.log(model)
     return model;
-    
-  };  
+  };
 };
