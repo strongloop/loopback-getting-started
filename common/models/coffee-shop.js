be0ce6ce81a7b7ac340e311dc74cf5bc627be604
Nora <nora.abdelgadir@ibm.com>
2019-05-10 15:01:04 -0400

    refactor: change callbacks to async functions

--- common/models/coffee-shop.js
+++ common/models/coffee-shop.js
@@ -4,19 +4,19 @@
 'use strict';
 
 module.exports = function(CoffeeShop) {
-  CoffeeShop.status = function(cb) {
-    var currentDate = new Date();
-    var currentHour = currentDate.getHours();
-    var OPEN_HOUR = 6;
-    var CLOSE_HOUR = 20;
+  CoffeeShop.status = async function() {
+    const currentDate = new Date();
+    const currentHour = currentDate.getHours();
+    const OPEN_HOUR = 6;
+    const CLOSE_HOUR = 20;
     console.log('Current hour is %d', currentHour);
-    var response;
+    let response;
     if (currentHour > OPEN_HOUR && currentHour < CLOSE_HOUR) {
       response = 'We are open for business.';
     } else {
       response = 'Sorry, we are closed. Open daily from 6am to 8pm.';
     }
-    cb(null, response);
+    return response;
   };
   CoffeeShop.remoteMethod(
     'status', {
--- server/boot/create-sample-models.js
+++ server/boot/create-sample-models.js
@@ -3,23 +3,19 @@
 
 'use strict';
 
-module.exports = function(app) {
-  app.dataSources.mysqlDs.automigrate('CoffeeShop', function(err) {
-    if (err) throw err;
+module.exports = async function(app) {
+  await app.dataSources.mysqlDs.automigrate('CoffeeShop');
+  const coffeeShops = await app.models.CoffeeShop.create([{
+    name: 'Bel Cafe',
+    city: 'Vancouver',
+  }, {
+    name: 'Three Bees Coffee House',
+    city: 'San Mateo',
+  }, {
+    name: 'Caffe Artigiano',
+    city: 'Vancouver',
+  },
+  ]);
 
-    app.models.CoffeeShop.create([{
-      name: 'Bel Cafe',
-      city: 'Vancouver',
-    }, {
-      name: 'Three Bees Coffee House',
-      city: 'San Mateo',
-    }, {
-      name: 'Caffe Artigiano',
-      city: 'Vancouver',
-    }], function(err, coffeeShops) {
-      if (err) throw err;
-
-      console.log('Models created: \n', coffeeShops);
-    });
-  });
+  console.log('Models created: \n', coffeeShops);
 };
